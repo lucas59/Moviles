c34e27d0940793f37d79a46a5a62caef2d9405e4
isma4102 <santoscastelvi2014@gmail.com>
2019-05-19 01:41:24 -0300

    lista de actores en perfil elemento

--- app/src/main/java/com/example/obligatoriomoviles/API/APIInterface.java
+++ app/src/main/java/com/example/obligatoriomoviles/API/APIInterface.java
@@ -1,6 +1,6 @@
 package com.example.obligatoriomoviles.API;
 
-import com.example.obligatoriomoviles.Clases.Peliculas;
+import com.example.obligatoriomoviles.Clases.Cine.Cine;
 import com.example.obligatoriomoviles.Clases.usuario;
 
 import retrofit2.Call;
@@ -11,10 +11,10 @@ import retrofit2.http.Query;
 
 public interface APIInterface {
     @GET(".")
-    Call<Peliculas> getImagen(@Query("sort_by") String sort_by,@Query("year") Integer año,@Query("language") String lenguaje,@Query("api_key") String key);
+    Call<Cine> getImagen(@Query("sort_by") String sort_by, @Query("year") Integer año, @Query("language") String lenguaje, @Query("api_key") String key);
 
     @GET("{id}")
-    Call<Peliculas> getPelicula(@Path("id") String id,@Query("api_key") String api_key,@Query("append_to_response") String filtro,@Query("language") String idioma);
+    Call<Cine> getPelicula(@Path("id") String id,@Query("api_key") String api_key,@Query("append_to_response") String filtro,@Query("language") String idioma);
 
 
    // @POST('')
